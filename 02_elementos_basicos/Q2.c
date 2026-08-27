9e33518ffe56680c69744e943a0de7f2ff1304e5
N. <dnaominishida@gmail.com>
2026-08-26 22:05:29 -0300

    corrigindo detalhes

--- 02_elementos_basicos/Q2.c
+++ 02_elementos_basicos/Q2.c
@@ -10,26 +10,28 @@ Exemplo de entrada:
 Exemplo de saída:
 Candidato A: 20%
 Candidato B: 15%
-Candidato C: 60%
-Nulos: 3%
+Candidato C: 61%
+Nulos: 4%
 */
 
 #include <stdio.h>
 
 int main() {
-    double c1, c2, c3, nulo, total;
-    scanf("%lf %lf %lf %lf", &c1, &c2, &c3, &nulo);
+    int c1, c2, c3, nulo, total;
+    double p1, p2, p3, pnulo;
+
+    scanf("%d %d %d %d", &c1, &c2, &c3, &nulo);
 
     total = c1+c2+c3+nulo;
-    c1 = (c1*100)/total;
-    c2 = (c2*100)/total;
-    c3 = (c3*100)/total;
-    nulo = (nulo*100)/total;
+    p1 = (c1*100.0)/total;
+    p2 = (c2*100.0)/total;
+    p3 = (c3*100.0)/total;
+    pnulo = (nulo*100.0)/total;
 
-    printf("Candidato A: %.0lf%%\n", c1);
-    printf("Candidato B: %.0lf%%\n", c2);
-    printf("Candidato C: %.0lf%%\n", c3);
-    printf("Nulos: %.0lf%%\n", nulo);
+    printf("Candidato A: %.0lf%%\n", p1);
+    printf("Candidato B: %.0lf%%\n", p2);
+    printf("Candidato C: %.0lf%%\n", p3);
+    printf("Nulos: %.0lf%%\n", pnulo);
 
     return 0;
 }
