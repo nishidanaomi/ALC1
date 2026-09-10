752e227898365aeb17216fc870d6c656f9a8d9be
Naomi Nishida <dnaominishida@gmail.com>
2026-09-10 20:03:02 -0300

    Update Q2.c

--- lista_02/Q2.c
+++ lista_02/Q2.c
@@ -1,8 +1,8 @@
-/*A Locadora de Veículos Eudora lançou uma grande promoção esse mês: pagando apenas R$90 por diária, o cliente pode alugar um carro de passeio. Para cada diária, o cliente recebe uma cota de quilometragem de 100Km. Cada quilômetro a mais custará uma taxa extra de R$12. Escreva um programa que receba como entrada a quantidade de dias e a quilometragem total rodada por um cliente dessa locadora e exiba o valor total a ser pago com duas casas decimais.*/
+/*A Locadora de VeÃ­culos Eudora lanÃ§ou uma grande promoÃ§Ã£o esse mÃªs: pagando apenas R$90 por diÃ¡ria, o cliente pode alugar um carro de passeio. Para cada diÃ¡ria, o cliente recebe uma cota de quilometragem de 100Km. Cada quilÃ´metro a mais custarÃ¡ uma taxa extra de R$12. Escreva um programa que receba como entrada a quantidade de dias e a quilometragem total rodada por um cliente dessa locadora e exiba o valor total a ser pago com duas casas decimais.*/
 
 #include <stdio.h>
-int main(){
 
+int main(){
     int dias, km;
     double valor;
 
